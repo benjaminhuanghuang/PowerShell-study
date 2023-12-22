f9d96ba0f970a4de827014250b0d08b8d72f98e7
BenjaminHuang <benjhuang@microsoft.com>
2023-12-22 08:57:57 -0800

    update

--- _code_sinppets/uninstall-app-uninstallstring.ps1
+++ _code_sinppets/uninstall-app-uninstallstring.ps1
@@ -1,6 +1,18 @@
-# Find package information, IdentifiyingNumber
+# Find package information, IdentifyingNumber
 # Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Microsoft Power BI Desktop (x64)"}
 
 # Find UninstallString in the registry
 # Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{7C5AE5FE-F899-43BB-BDDC-DB1AED58B5D9}
-MsiExec.exe /X{7C5AE5FE-F899-43BB-BDDC-DB1AED58B5D9}
+
+#$app = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Microsoft Power BI Desktop (x64)"}
+#$id = Get-WmiObject -Class Win32_Product | Where-Object Name -eq 'Microsoft Power BI Desktop (x64)' | Select-Object -ExpandProperty IdentifyingNumber
+
+$app = Get-WmiObject -Class Win32_Product -Filter "Name = 'Microsoft Power BI Desktop (x64)'"
+if ($app) {
+    # Power BI Desktop is installed, uninstall it
+    Write-Host "Uninstalling Power BI Desktop..."
+	msiexec /x $app.IdentifyingNumber
+} else {
+    # Power BI Desktop is not installed
+    Write-Host "Power BI Desktop is not installed on this computer."
+}
